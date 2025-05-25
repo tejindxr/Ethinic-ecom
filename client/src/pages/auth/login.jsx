38f29af367c8721ee5872e503182f82e43a501c1
Ishan-Mangla <your-email@example.com>
2025-05-25 19:06:05 +0530

    ishan

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -37,7 +37,7 @@ function AuthLogin() {
     <div className="mx-auto w-full max-w-md space-y-6">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Sign in to your account
+          Sign in to your account ishan
         </h1>
         <p className="mt-2">
           Don't have an account
